6f915db73fadf3dfac14753c7d89e2d3f0da7974
SekaiW <92559126+SekaiW@users.noreply.github.com>
2024-02-24 01:16:20 -0500

    Class and File Reader

--- src/main/kotlin/Cell.kt
+++ src/main/kotlin/Cell.kt
@@ -1,5 +1,55 @@
-package classes
+package com.sekaiw.alpkotlin
 
-class Cell {
+import com.opencsv.CSVReader
+import java.io.FileReader
+
+data class Cell (
+    var oem: String,
+    var model: String,
+    var bodyDimensions: String,
+    var displayType: String,
+    var displayResolution: String
+
+) {
+    var launchAnnounced: String = " "
+        get() = field
+        set(value) { field = value } // to Int
+    var launchStatus: String = " "
+        get() = field
+        set(value) { field = value }
+    var bodyWeight: String = " "
+        get() = field
+        set(value) { field = value } // to Float
+    var bodySim: String = " "
+        get() = field
+        set(value) { field = value }
+    var displaySize: String = " "
+        get() = field
+        set(value) { field = value } // to Float
+    var featuresSensors: String = " "
+        get() = field
+        set(value) { field = value }
+    var platformOS: String = " "
+        get() = field
+        set(value) { field = value }
 
 } // end of Cell
+
+fun fileReader() {
+    val path = "src/main/resources/cells.csv"
+
+    try {
+        FileReader(path).use { fileReader ->
+            val reader = CSVReader(fileReader)
+            var row: Array<String>?
+
+            while (reader.readNext().also { row = it } != null) {
+                for (column in row!!) { // print each column
+                    println(column)
+                }
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+} // end of fileReader
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,10 @@ Name: Sekai Wynn
 Date: April 16, 2024
 */
 
-import classes.Cell as Cell
+import com.sekaiw.alpkotlin.fileReader
+
 fun main() {
+
+    fileReader() // calling for csv file to be read
 
 } // end of main
